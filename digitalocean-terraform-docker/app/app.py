bfc5a54ee565ebdbf99c2261d0afb02c99110cce
Marcus Lind <marcuslind90@gmail.com>
2018-12-10 22:12:21 +0700

    App now use uuid

--- digitalocean-terraform-docker/app/app.py
+++ digitalocean-terraform-docker/app/app.py
@@ -1,9 +1,10 @@
-import os
+import uuid
 from flask import Flask
 app = Flask(__name__)
+app.config.setdefault("id", str(uuid.uuid4()))
 
 
 @app.route("/")
 def index():
-    app_count = os.environ.get("APP_COUNT", "Undefined")
-    return f"Hello World from #{app_count}"
+    app_id = app.config.get("id")
+    return f"Hello World from {app_id}"
